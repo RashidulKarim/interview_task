77550821887f4d030cfd94289b2a6b08264113d6
rashidulkarim <rashidul.karim7@gmail.com>
2021-12-10 00:52:53 +0600

    working

--- src/pages/List/List.js
+++ src/pages/List/List.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import './list.css';
 const List = () => {
     const [listData, setListData] = useState({})
@@ -6,6 +7,7 @@ const List = () => {
     const [query, setQuery] = useState('')
     const [searchingField, setSearchingFiled] = useState("id")
     const [order, setOrder] = useState("ASC")
+    const [message, setMessage] = useState({})
     
     useEffect(()=>{
         fetch("http://localhost/api/list.php")
@@ -30,12 +32,23 @@ const List = () => {
             setOrder("ASC")
         }
     }
-
-    // listData?.headers?.[0] &&console.log(Object.keys(listData?.headers?.[0]).map((th, i) =>console.log(th)))
     
 
     const search = (rows) => {
         return rows.filter(row => (row[searchingField] || " ").toString().toLowerCase().indexOf(query.toLocaleLowerCase())> -1)
+    }
+
+    const handleDragEnd = (results) => { 
+        if(!results.destination) return     
+        let tempUser = [...rowData]
+        let [selectRow] = tempUser.splice(results.source.index, 1)
+        tempUser.splice(results.destination.index, 0, selectRow )        
+        setRowData(tempUser)
+        if(results.source["index"] !== results.destination["index"]){
+            fetch("http://localhost/api/reorder.php")
+            .then(res => res.json())
+            .then(data => setMessage(data))
+        }
     }
     
     return (
@@ -51,6 +64,7 @@ const List = () => {
                         listData?.headers?.[0] && Object.keys(listData?.headers?.[0]).map((th, i) =>listData?.headers?.[0][th].searchable === true && <option key={i} value={th}>{listData?.headers?.[0][th].title}</option>)
                     }
                 </select>
+            <DragDropContext onDragEnd={(results) => handleDragEnd(results)}>
             <table>
                 <thead>
                     <tr>
@@ -60,39 +74,44 @@ const List = () => {
                     }
                     </tr>
                 </thead>
-                <tbody>
+                <Droppable droppableId='tbody'>
+                {
+                   (provided)=>(
+                    <tbody ref={provided.innerRef} {...provided.droppableProps}>
                    
-                    {
-                       rowData && search(rowData).map((data,i)=> <tr key={i}>
-                           {Object.keys(listData?.headers?.[0]).map((fieldName, i) =>listData?.headers?.[0][fieldName].hidden === false &&  <td key={i}>{data[fieldName]}</td>)}
-                       </tr>
-                       
-                       )      
-                    }
-                </tbody>
+                        {
+                        rowData && search(rowData).map((data,i)=> 
+                        <Draggable key={data.name} draggableId={data.name} index={i}>
+                           {
+                               (provided) => (
+                                <tr key={i}
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                
+                                >
+                                {Object.keys(listData?.headers?.[0]).map((fieldName, i) =>listData?.headers?.[0][fieldName].hidden === false &&  
+                                <td key={i} 
+                                {...provided.dragHandleProps}>{data[fieldName]}</td>)}
+                            </tr>
+                               )
+                           }
+                        </Draggable>
+                        
+                        )      
+                        }
+                        {provided.placeholder}
+                    </tbody>
+                   ) 
+                }
+                </Droppable>
             </table>
+            </DragDropContext>
             </div>
+            {
+                Object.keys(message).length> 0 && message["status"] === "success"? <p style={{textAlign:'center', color:'green'}} >{message.messages}</p> : <p style={{textAlign:'center', color:'red'}} >{message.messages}</p>
+            }
         </div>
     );
 };
 
 export default List;
-
-
-// <tr key={i}>
-//                            {/* {
-//                                console.log(Object.keys(listData?.headers?.[0]))
-//                            } */}
-//                            {
-//                                console.log("row",Object.keys(rowData))
-//                            }
-                           
-//                            {/* {
-//                                Object.keys(rowData).map((columnData, i) => <td key={i}>{Object.keys(listData?.headers?.[0])[columnData]}</td>
-//                                )
-//                            } */}
-//                            {
-//                                Object.keys(listData?.headers?.[0]).map(columnName => console.log(columnName)
-//                                 )
-//                            }
-//                        </tr>
